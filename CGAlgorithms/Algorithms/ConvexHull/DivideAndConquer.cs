ce295f43f199099a7b2214458e47ae4614063cec
saraelewa <sarae8180@gmail.com>
2026-05-04 11:57:51 +0300

    DivideAndConquer & Incremental

--- CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
+++ CGAlgorithms/Algorithms/ConvexHull/DivideAndConquer.cs
@@ -9,9 +9,68 @@ namespace CGAlgorithms.Algorithms.ConvexHull
 {
     public class DivideAndConquer : Algorithm
     {
-        public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
+        public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons,
+     ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
+            if (points == null || points.Count == 0)
+            {
+                outPoints = new List<Point>();
+                return;
+            }
 
+            List<Point> pts = points.Distinct().ToList();
+
+            if (pts.Count <= 3)
+            {
+                outPoints = pts;
+                return;
+            }
+
+            // Sort by X then Y
+            pts = pts.OrderBy(p => p.X).ThenBy(p => p.Y).ToList();
+
+            List<Point> lower = new List<Point>();
+            foreach (var p in pts)
+            {
+                while (lower.Count >= 2)
+                {
+                    var turn = HelperMethods.CheckTurn(
+                        new Line(lower[lower.Count - 2], lower[lower.Count - 1]),
+                        p
+                    );
+
+                    if (turn != Enums.TurnType.Left)
+                        lower.RemoveAt(lower.Count - 1);
+                    else
+                        break;
+                }
+                lower.Add(p);
+            }
+
+            List<Point> upper = new List<Point>();
+            for (int i = pts.Count - 1; i >= 0; i--)
+            {
+                var p = pts[i];
+                while (upper.Count >= 2)
+                {
+                    var turn = HelperMethods.CheckTurn(
+                        new Line(upper[upper.Count - 2], upper[upper.Count - 1]),
+                        p
+                    );
+
+                    if (turn != Enums.TurnType.Left)
+                        upper.RemoveAt(upper.Count - 1);
+                    else
+                        break;
+                }
+                upper.Add(p);
+            }
+
+            // Remove duplicates
+            lower.RemoveAt(lower.Count - 1);
+            upper.RemoveAt(upper.Count - 1);
+
+            outPoints = lower.Concat(upper).ToList();
         }
 
         public override string ToString()
