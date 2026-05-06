b7109cec6dff566f7ca9e91370f1196f98d6ca26
saraelewa <sarae8180@gmail.com>
2026-05-06 13:42:01 +0300

    done bgd

--- CGAlgorithms/Algorithms/PolygonTriangulation/SubtractingEars.cs
+++ CGAlgorithms/Algorithms/PolygonTriangulation/SubtractingEars.cs
@@ -1,22 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using CGUtilities;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CGAlgorithms.Algorithms.PolygonTriangulation
 {
-    class SubtractingEars : Algorithm
+    public class SubtractingEars : Algorithm
     {
-        public override void Run(List<Point> points, List<Line> lines,
-            List<Polygon> polygons,
-            ref List<Point> outPoints,
-            ref List<Line> outLines,
-            ref List<Polygon> outPolygons)
+        public override void Run(List<CGUtilities.Point> points, List<CGUtilities.Line> lines, List<CGUtilities.Polygon> polygons, ref 
+List<CGUtilities.Point> outPoints,ref List<CGUtilities.Line> outLines,ref List<CGUtilities.Polygon> outPolygons)
         {
-            outLines = new List<Line>();
+            outLines = new List<CGUtilities.Line>();
+            outPoints = new List<CGUtilities.Point>();
+            outPolygons = new List<CGUtilities.Polygon>();
 
-            List<Point> poly = new List<Point>(points);
+            if (points == null || points.Count < 3)
+                return;
 
+            List<CGUtilities.Point> poly = new List<CGUtilities.Point>(points);
+
+            // Make polygon counter-clockwise
             if (PolygonArea(poly) < 0)
                 poly.Reverse();
 
@@ -26,45 +30,48 @@ namespace CGAlgorithms.Algorithms.PolygonTriangulation
 
                 for (int i = 0; i < poly.Count; i++)
                 {
-                    Point prev = poly[(i - 1 + poly.Count) % poly.Count];
-                    Point curr = poly[i];
-                    Point next = poly[(i + 1) % poly.Count];
+                    CGUtilities.Point prev = poly[(i - 1 + poly.Count) % poly.Count];
+                    CGUtilities.Point curr = poly[i];
+                    CGUtilities.Point next = poly[(i + 1) % poly.Count];
 
-                    if (IsConvex(prev, curr, next))
+                    // Ear must be convex
+                    if (!IsConvex(prev, curr, next))
+                        continue;
+
+                    bool hasPointInside = false;
+
+                    for (int j = 0; j < poly.Count; j++)
                     {
-                        bool hasPointInside = false;
+                        CGUtilities.Point testPoint = poly[j];
 
-                        for (int j = 0; j < poly.Count; j++)
+                        if (SamePoint(testPoint, prev) ||
+                            SamePoint(testPoint, curr) ||
+                            SamePoint(testPoint, next))
+                            continue;
+
+                        if (PointInTriangle(testPoint, prev, curr, next))
                         {
-                            if (poly[j] == prev ||
-                                poly[j] == curr ||
-                                poly[j] == next)
-                                continue;
-
-                            if (PointInTriangle(poly[j], prev, curr, next))
-                            {
-                                hasPointInside = true;
-                                break;
-                            }
-                        }
-
-                        if (!hasPointInside)
-                        {
-                            // add triangulation diagonal only
-                            if (!IsPolygonEdge(prev, next, poly))
-                            {
-                                outLines.Add(new Line(prev, next));
-                            }
-
-                            // remove ear vertex
-                            poly.RemoveAt(i);
-
-                            earFound = true;
+                            hasPointInside = true;
                             break;
                         }
                     }
+
+                    // Found ear
+                    if (!hasPointInside)
+                    {
+                        if (!IsPolygonEdge(prev, next, poly))
+                        {
+                            outLines.Add(new CGUtilities.Line(prev, next));
+                        }
+
+                        poly.RemoveAt(i);
+
+                        earFound = true;
+                        break;
+                    }
                 }
 
+                // Safety check
                 if (!earFound)
                     break;
             }
@@ -75,33 +82,37 @@ namespace CGAlgorithms.Algorithms.PolygonTriangulation
             return "Subtracting Ears";
         }
 
-        private double PolygonArea(List<Point> pts)
+        private double PolygonArea(List<CGUtilities.Point> pts)
         {
             double area = 0;
 
             for (int i = 0; i < pts.Count; i++)
             {
-                Point p1 = pts[i];
-                Point p2 = pts[(i + 1) % pts.Count];
+                CGUtilities.Point p1 = pts[i];
+                CGUtilities.Point p2 = pts[(i + 1) % pts.Count];
 
-                area += (p1.X * p2.Y - p2.X * p1.Y);
+                area += (p1.X * p2.Y) - (p2.X * p1.Y);
             }
 
             return area / 2.0;
         }
 
-        private bool IsConvex(Point a, Point b, Point c)
+        private bool IsConvex(CGUtilities.Point a, CGUtilities.Point b, CGUtilities.Point c)
         {
             return Cross(a, b, c) > 0;
         }
 
-        private double Cross(Point a, Point b, Point c)
+        private double Cross(CGUtilities.Point a, CGUtilities.Point b, CGUtilities.Point c)
         {
-            return (b.X - a.X) * (c.Y - a.Y) -
-                   (b.Y - a.Y) * (c.X - a.X);
+            return ((b.X - a.X) * (c.Y - a.Y)) -
+                   ((b.Y - a.Y) * (c.X - a.X));
         }
 
-        private bool PointInTriangle(Point p, Point a, Point b, Point c)
+        private bool PointInTriangle(
+            CGUtilities.Point p,
+            CGUtilities.Point a,
+            CGUtilities.Point b,
+            CGUtilities.Point c)
         {
             double d1 = Cross(p, a, b);
             double d2 = Cross(p, b, c);
@@ -113,19 +124,30 @@ namespace CGAlgorithms.Algorithms.PolygonTriangulation
             return !(hasNeg && hasPos);
         }
 
-        private bool IsPolygonEdge(Point a, Point b, List<Point> poly)
+        private bool IsPolygonEdge(
+            CGUtilities.Point a,
+            CGUtilities.Point b,
+            List<CGUtilities.Point> poly)
         {
             for (int i = 0; i < poly.Count; i++)
             {
-                Point p1 = poly[i];
-                Point p2 = poly[(i + 1) % poly.Count];
+                CGUtilities.Point p1 = poly[i];
+                CGUtilities.Point p2 = poly[(i + 1) % poly.Count];
 
-                if ((p1 == a && p2 == b) ||
-                    (p1 == b && p2 == a))
+                if ((SamePoint(p1, a) && SamePoint(p2, b)) ||
+                    (SamePoint(p1, b) && SamePoint(p2, a)))
+                {
                     return true;
+                }
             }
 
             return false;
+        }
+
+        private bool SamePoint(CGUtilities.Point p1, CGUtilities.Point p2)
+        {
+            return Math.Abs(p1.X - p2.X) < 1e-9 &&
+                   Math.Abs(p1.Y - p2.Y) < 1e-9;
         }
     }
 }
--- CGAlgorithms/Algorithms/SegmentIntersection/SweepLine.cs
+++ CGAlgorithms/Algorithms/SegmentIntersection/SweepLine.cs
@@ -1,28 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using CGUtilities;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CGAlgorithms.Algorithms.SegmentIntersection
 {
-    class SweepLine : Algorithm
+    public class SweepLine:Algorithm
     {
-        public override void Run(List<Point> points, List<Line> lines,
-            List<Polygon> polygons,
-            ref List<Point> outPoints,
-            ref List<Line> outLines,
-            ref List<Polygon> outPolygons)
+        public override void Run(List<CGUtilities.Point> points, List<CGUtilities.Line> lines, List<CGUtilities.Polygon> polygons, ref
+ List<CGUtilities.Point> outPoints, ref List<CGUtilities.Line> outLines, ref List<CGUtilities.Polygon> outPolygons)
         {
-            outPoints = new List<Point>();
+            outPoints = new List<CGUtilities.Point>();
+            outLines = new List<CGUtilities.Line>();
+            outPolygons = new List<CGUtilities.Polygon>();
 
-            // Sort lines based on minimum X coordinate
-            var sortedLines = lines.OrderBy(l => Math.Min(l.Start.X, l.End.X)).ToList();
+            // Sort lines by minimum X
+            List<CGUtilities.Line> sortedLines =
+                lines.OrderBy(l => Math.Min(l.Start.X, l.End.X)).ToList();
 
             for (int i = 0; i < sortedLines.Count; i++)
             {
                 for (int j = i + 1; j < sortedLines.Count; j++)
                 {
-                    Point intersection;
+                    CGUtilities.Point intersection;
 
                     if (SegmentsIntersect(sortedLines[i], sortedLines[j], out intersection))
                     {
@@ -37,14 +38,18 @@ namespace CGAlgorithms.Algorithms.SegmentIntersection
             return "Sweep Line";
         }
 
-        private bool SegmentsIntersect(Line l1, Line l2, out Point intersection)
+        private bool SegmentsIntersect(
+            CGUtilities.Line l1,
+            CGUtilities.Line l2,
+            out CGUtilities.Point intersection)
         {
-            intersection = new Point(0, 0);
+            intersection = new CGUtilities.Point(0, 0);
 
-            Point p1 = l1.Start;
-            Point q1 = l1.End;
-            Point p2 = l2.Start;
-            Point q2 = l2.End;
+            CGUtilities.Point p1 = l1.Start;
+            CGUtilities.Point q1 = l1.End;
+
+            CGUtilities.Point p2 = l2.Start;
+            CGUtilities.Point q2 = l2.End;
 
             int o1 = Orientation(p1, q1, p2);
             int o2 = Orientation(p1, q1, q2);
@@ -61,42 +66,53 @@ namespace CGAlgorithms.Algorithms.SegmentIntersection
             return false;
         }
 
-        private int Orientation(Point p, Point q, Point r)
+        private int Orientation(
+            CGUtilities.Point p,
+            CGUtilities.Point q,
+            CGUtilities.Point r)
         {
-            double val = (q.Y - p.Y) * (r.X - q.X) -
-                         (q.X - p.X) * (r.Y - q.Y);
+            double val =
+                (q.Y - p.Y) * (r.X - q.X) -
+                (q.X - p.X) * (r.Y - q.Y);
 
             if (Math.Abs(val) < 1e-9)
-                return 0; // Collinear
+                return 0;
 
-            return (val > 0) ? 1 : 2; // Clockwise or Counterclockwise
+            return (val > 0) ? 1 : 2;
         }
 
-        private Point GetIntersectionPoint(Line l1, Line l2)
+        private CGUtilities.Point GetIntersectionPoint(
+            CGUtilities.Line l1,
+            CGUtilities.Line l2)
         {
             double x1 = l1.Start.X;
             double y1 = l1.Start.Y;
+
             double x2 = l1.End.X;
             double y2 = l1.End.Y;
 
             double x3 = l2.Start.X;
             double y3 = l2.Start.Y;
+
             double x4 = l2.End.X;
             double y4 = l2.End.Y;
 
-            double denom = (x1 - x2) * (y3 - y4) -
-                           (y1 - y2) * (x3 - x4);
+            double denom =
+                (x1 - x2) * (y3 - y4) -
+                (y1 - y2) * (x3 - x4);
 
             if (Math.Abs(denom) < 1e-9)
-                return new Point(0, 0);
+                return new CGUtilities.Point(0, 0);
 
-            double px = ((x1 * y2 - y1 * x2) * (x3 - x4) -
-                        (x1 - x2) * (x3 * y4 - y3 * x4)) / denom;
+            double px =
+                ((x1 * y2 - y1 * x2) * (x3 - x4) -
+                (x1 - x2) * (x3 * y4 - y3 * x4)) / denom;
 
-            double py = ((x1 * y2 - y1 * x2) * (y3 - y4) -
-                        (y1 - y2) * (x3 * y4 - y3 * x4)) / denom;
+            double py =
+                ((x1 * y2 - y1 * x2) * (y3 - y4) -
+                (y1 - y2) * (x3 * y4 - y3 * x4)) / denom;
 
-            return new Point(px, py);
+            return new CGUtilities.Point(px, py);
         }
     }
 }
