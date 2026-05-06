d098ecbf3cbc38e5fb2fed897f3c9e6b364201bf
saraelewa <sarae8180@gmail.com>
2026-05-06 09:56:04 +0300

    done

--- CGAlgorithms/Algorithms/PolygonTriangulation/SubtractingEars.cs
+++ CGAlgorithms/Algorithms/PolygonTriangulation/SubtractingEars.cs
@@ -1,21 +1,131 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CGUtilities;
 
 namespace CGAlgorithms.Algorithms.PolygonTriangulation
 {
     class SubtractingEars : Algorithm
     {
-        public override void Run(List<CGUtilities.Point> points, List<CGUtilities.Line> lines, List<CGUtilities.Polygon> polygons, ref List<CGUtilities.Point> outPoints, ref List<CGUtilities.Line> outLines, ref List<CGUtilities.Polygon> outPolygons)
+        public override void Run(List<Point> points, List<Line> lines,
+            List<Polygon> polygons,
+            ref List<Point> outPoints,
+            ref List<Line> outLines,
+            ref List<Polygon> outPolygons)
         {
+            outLines = new List<Line>();
 
+            List<Point> poly = new List<Point>(points);
+
+            if (PolygonArea(poly) < 0)
+                poly.Reverse();
+
+            while (poly.Count > 3)
+            {
+                bool earFound = false;
+
+                for (int i = 0; i < poly.Count; i++)
+                {
+                    Point prev = poly[(i - 1 + poly.Count) % poly.Count];
+                    Point curr = poly[i];
+                    Point next = poly[(i + 1) % poly.Count];
+
+                    if (IsConvex(prev, curr, next))
+                    {
+                        bool hasPointInside = false;
+
+                        for (int j = 0; j < poly.Count; j++)
+                        {
+                            if (poly[j] == prev ||
+                                poly[j] == curr ||
+                                poly[j] == next)
+                                continue;
+
+                            if (PointInTriangle(poly[j], prev, curr, next))
+                            {
+                                hasPointInside = true;
+                                break;
+                            }
+                        }
+
+                        if (!hasPointInside)
+                        {
+                            // add triangulation diagonal only
+                            if (!IsPolygonEdge(prev, next, poly))
+                            {
+                                outLines.Add(new Line(prev, next));
+                            }
+
+                            // remove ear vertex
+                            poly.RemoveAt(i);
+
+                            earFound = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!earFound)
+                    break;
+            }
         }
 
         public override string ToString()
         {
             return "Subtracting Ears";
+        }
+
+        private double PolygonArea(List<Point> pts)
+        {
+            double area = 0;
+
+            for (int i = 0; i < pts.Count; i++)
+            {
+                Point p1 = pts[i];
+                Point p2 = pts[(i + 1) % pts.Count];
+
+                area += (p1.X * p2.Y - p2.X * p1.Y);
+            }
+
+            return area / 2.0;
+        }
+
+        private bool IsConvex(Point a, Point b, Point c)
+        {
+            return Cross(a, b, c) > 0;
+        }
+
+        private double Cross(Point a, Point b, Point c)
+        {
+            return (b.X - a.X) * (c.Y - a.Y) -
+                   (b.Y - a.Y) * (c.X - a.X);
+        }
+
+        private bool PointInTriangle(Point p, Point a, Point b, Point c)
+        {
+            double d1 = Cross(p, a, b);
+            double d2 = Cross(p, b, c);
+            double d3 = Cross(p, c, a);
+
+            bool hasNeg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+            bool hasPos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+            return !(hasNeg && hasPos);
+        }
+
+        private bool IsPolygonEdge(Point a, Point b, List<Point> poly)
+        {
+            for (int i = 0; i < poly.Count; i++)
+            {
+                Point p1 = poly[i];
+                Point p2 = poly[(i + 1) % poly.Count];
+
+                if ((p1 == a && p2 == b) ||
+                    (p1 == b && p2 == a))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
--- CGAlgorithms/Algorithms/SegmentIntersection/SweepLine.cs
+++ CGAlgorithms/Algorithms/SegmentIntersection/SweepLine.cs
@@ -1,21 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CGUtilities;
 
 namespace CGAlgorithms.Algorithms.SegmentIntersection
 {
-    class SweepLine:Algorithm
+    class SweepLine : Algorithm
     {
-        public override void Run(List<CGUtilities.Point> points, List<CGUtilities.Line> lines, List<CGUtilities.Polygon> polygons, ref List<CGUtilities.Point> outPoints, ref List<CGUtilities.Line> outLines, ref List<CGUtilities.Polygon> outPolygons)
+        public override void Run(List<Point> points, List<Line> lines,
+            List<Polygon> polygons,
+            ref List<Point> outPoints,
+            ref List<Line> outLines,
+            ref List<Polygon> outPolygons)
         {
+            outPoints = new List<Point>();
 
+            // Sort lines based on minimum X coordinate
+            var sortedLines = lines.OrderBy(l => Math.Min(l.Start.X, l.End.X)).ToList();
+
+            for (int i = 0; i < sortedLines.Count; i++)
+            {
+                for (int j = i + 1; j < sortedLines.Count; j++)
+                {
+                    Point intersection;
+
+                    if (SegmentsIntersect(sortedLines[i], sortedLines[j], out intersection))
+                    {
+                        outPoints.Add(intersection);
+                    }
+                }
+            }
         }
 
         public override string ToString()
         {
             return "Sweep Line";
+        }
+
+        private bool SegmentsIntersect(Line l1, Line l2, out Point intersection)
+        {
+            intersection = new Point(0, 0);
+
+            Point p1 = l1.Start;
+            Point q1 = l1.End;
+            Point p2 = l2.Start;
+            Point q2 = l2.End;
+
+            int o1 = Orientation(p1, q1, p2);
+            int o2 = Orientation(p1, q1, q2);
+            int o3 = Orientation(p2, q2, p1);
+            int o4 = Orientation(p2, q2, q1);
+
+            // General case
+            if (o1 != o2 && o3 != o4)
+            {
+                intersection = GetIntersectionPoint(l1, l2);
+                return true;
+            }
+
+            return false;
+        }
+
+        private int Orientation(Point p, Point q, Point r)
+        {
+            double val = (q.Y - p.Y) * (r.X - q.X) -
+                         (q.X - p.X) * (r.Y - q.Y);
+
+            if (Math.Abs(val) < 1e-9)
+                return 0; // Collinear
+
+            return (val > 0) ? 1 : 2; // Clockwise or Counterclockwise
+        }
+
+        private Point GetIntersectionPoint(Line l1, Line l2)
+        {
+            double x1 = l1.Start.X;
+            double y1 = l1.Start.Y;
+            double x2 = l1.End.X;
+            double y2 = l1.End.Y;
+
+            double x3 = l2.Start.X;
+            double y3 = l2.Start.Y;
+            double x4 = l2.End.X;
+            double y4 = l2.End.Y;
+
+            double denom = (x1 - x2) * (y3 - y4) -
+                           (y1 - y2) * (x3 - x4);
+
+            if (Math.Abs(denom) < 1e-9)
+                return new Point(0, 0);
+
+            double px = ((x1 * y2 - y1 * x2) * (x3 - x4) -
+                        (x1 - x2) * (x3 * y4 - y3 * x4)) / denom;
+
+            double py = ((x1 * y2 - y1 * x2) * (y3 - y4) -
+                        (y1 - y2) * (x3 * y4 - y3 * x4)) / denom;
+
+            return new Point(px, py);
         }
     }
 }
